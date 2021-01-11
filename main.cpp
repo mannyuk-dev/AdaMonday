da9478709853f18a58b9c04b7b5a85948aefdafd
Emmanuel Antwi <emmanuel.antwi@ada.ac.uk>
2021-01-11 12:52:06 +0000

    MAde exerceise 1

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Hello World!\n";
+  std::string quote;
+  std::string author;
+  std::cout << "What is the quote\n";
+  //std::cin >> quote;
+  getline(std::cin, quote);
+  std:: cout << "Who said it\n";
+  //std::cin >> author;
+  getline(std::cin , author);
+  std::cout << "\n" << author << " says: \"" << quote << "\"\n";
+    return 0;
 }
