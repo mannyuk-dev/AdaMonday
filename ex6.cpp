6035553ba047b17c2898fdb2e92d43e9b3ef81ed
mannyuk-dev <56549229+mannyuk-dev@users.noreply.github.com>
2021-01-11 14:48:32 +0000

    Create ex6.cpp

---
+++ ex6.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+    int pulse;
+
+    cout << "Please enter a your age: ";
+    cin >> age;
+
+    cout << "Please enter resting pulse: ";
+    cin >> pulse;
+
+    cout << "\n\nResting pulse: " << pulse << "\tAge: " << age;
+
+
+    cout << "\n\nIntensity\t\tRate ";
+    cout << "\n.....................................\n";
+
+    double count = 0.4;
+
+    for (int i = 0; i < 12; i++) {
+
+      int rate = (( 220 - age - pulse) *  count) + pulse;
+
+      cout << count * 100  << "%" << "\t\t\t\t" << rate << "\n";
+      count = count + 0.05;
+
+    }
+}
